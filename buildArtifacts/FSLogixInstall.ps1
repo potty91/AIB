a0d9a9c1e022f5051168185b5a0a580d6f93438f
potty91 <43879431+potty91@users.noreply.github.com>
2020-05-15 15:27:10 +0100

    Added Fslogix Install Script and archived legacy files

---
+++ buildArtifacts/FSLogixInstall.ps1
@@ -0,0 +1,5 @@
+﻿$CmdPath = "C:\Install\FSLogixAppsSetup.exe"
+$cmdArgList = "/install /passive /norestart /log`"C:\Logs\FSLogix\FSlogix.txt`\"""
+#$cmdArgList = "/install"
+
+Start-Process -FilePath $cmdPath -ArgumentList $cmdArgList -PassThru | Wait-Process -Timeout 600
