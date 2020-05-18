6eea7b52273677027915d081a2dc6b5e3ff2bd5f
potty91 <43879431+potty91@users.noreply.github.com>
2020-05-18 09:23:53 +0000

    Add files via upload

---
+++ buildArtifacts/OfficeInstall.ps1
@@ -0,0 +1,9 @@
+﻿$CmdPath = "C:\Install\setup.exe"
+
+$cmdArgList = "/configure `"C:\Install\Configuration.xml`\"""
+
+#$cmdArgList = "/install"
+
+
+
+Start-Process -FilePath $cmdPath -ArgumentList $cmdArgList -PassThru | Wait-Process -Timeout 1800
