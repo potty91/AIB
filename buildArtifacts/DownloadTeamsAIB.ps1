5bd6ccc57f0000f939ce7af8284af9ad9b361ec4
potty91 <43879431+potty91@users.noreply.github.com>
2020-06-07 10:45:43 +0100

    Update DownloadTeamsAIB.ps1

--- buildArtifacts/DownloadTeamsAIB.ps1
+++ buildArtifacts/DownloadTeamsAIB.ps1
@@ -1,1 +1,8 @@
+
+# Setup directory
+New-Item C:\Source -type Directory
+
+# Copy over build artifacts
+Copy-Item "C:\buildArtifacts\*" -Destination "C:\Source" -Recurse
+
 Invoke-WebRequest -Uri "https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.4461/Teams_windows_x64.msi" -OutFile "C:\Teams_windows_x64.msi"
